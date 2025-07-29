27d1ee0122ce93bad9979e10781f36bcc8cbb440
Karun-Dixit <karundixit5@gmail.com>
2025-07-29 18:28:50 +0545

    deleted

--- routes/stripeRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createCheckoutSession } from '../controllers/stripeController.js';
-
-const router = express.Router();
-
-// Stripe Checkout endpoint
-router.post('/create-checkout-session', createCheckoutSession);
-
-export default router;
